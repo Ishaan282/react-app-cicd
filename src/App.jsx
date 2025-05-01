553f1b47e904021735ef8881c32cac9654fd0b09
Ishaan282 <ishaansingla282@gmail.com>
2025-05-01 20:32:06 +0530

    hi

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>react app implement using Vite pipeline </h2>
+      <h3>with github actions</h3>
+      <h1>created by Ishaan</h1>
+      <p></p>
+    </>
+  )
+}
+
+export default App
